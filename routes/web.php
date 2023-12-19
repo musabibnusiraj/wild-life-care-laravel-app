d094aa30cf3e9efd3ede64dcad4214fd0d9012fa
mohamedarsath85 <arshath@clancodelabs.com>
2023-12-19 17:17:35 +0530

    create inverstigation page

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SocialiteController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Complain;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/complain', [Complain::class, 'complain']);
 
 Route::get('/login/callback', [SocialiteController::class, 'callback']);
 
